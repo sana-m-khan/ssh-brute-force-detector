90138ba7cec1569d18cd0dce364438e36d2ff5d0
sana-m-khan <khan.mt.sana@gmail.com>
2026-06-17 22:35:15 +0000

    Adding auto-unblock feature after a set amount of time (24 hrs default)

--- src/detector.py
+++ src/detector.py
@@ -2,9 +2,12 @@ import re
 import time
 import subprocess
 from collections import defaultdict
+from datetime import datetime, timedelta
 
 failed_attempts = defaultdict(int)
-whitelist = ["192.168.64.1"]
+blocked_ips = {}
+whitelist = []
+duration = timedelta(hours = 24)
 with open("/var/log/auth.log", "r") as file:
 	file.seek(0, 2)
 	while True:
@@ -19,6 +22,16 @@ with open("/var/log/auth.log", "r") as file:
 					failed_attempts[ip] += 1
 					if failed_attempts[ip] >= 5:
 						print(f"ALERT: {ip} has {failed_attempts[ip]} failed attempts.")
+						now = datetime.now()
 						subprocess.run(["sudo", "iptables", "-A", "INPUT", "-s", ip, "-p", "tcp", "--dport", "22", "-j", "DROP"])
+						blocked_ips[ip] = now
+						with open("blocked_ips.log", "a") as blockedlog:
+							blockedlog.write(f"{ip} has been blocked as of {now}\n")
 		else:
+			for key in list(blocked_ips):
+				if datetime.now() - blocked_ips[key] > duration:
+					subprocess.run(["sudo", "iptables", "-D", "INPUT", "-s", key, "-p", "tcp", "--dport", "22", "-j", "DROP"])
+					del blocked_ips[key]
+					del failed_attempts[key]
 			time.sleep(1)
+
