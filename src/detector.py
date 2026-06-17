e6ab8f6053c6bbbfa1394a9f4d34b47b5002c91d
sana-m-khan <khan.mt.sana@gmail.com>
2026-06-17 17:51:55 +0000

    adding continous monitoring to accurately detect when someone reaches the threshold

--- src/detector.py
+++ src/detector.py
@@ -1,15 +1,19 @@
 import re
+import time
+from collections import defaultdict
 
-failed_attempts = {}
+failed_attempts = defaultdict(int)
 with open("/var/log/auth.log", "r") as file:
-	for line in file:
-		if "Failed password" in line:
-			match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
-			if match:
-				ip = match.group(1)
-				if ip in failed_attempts:
+	file.seek(0, 2)
+	while True:
+		line = file.readline()
+		if line:
+			if "Failed password" in line:
+				match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
+				if match:
+					ip = match.group(1)
 					failed_attempts[ip] += 1
-				else:
-					failed_attempts[ip] = 1
-				if failed_attempts[ip] >= 5:
-					print(f"ALERT: {ip} has {failed_attempts[ip]} failed attempts.")
+					if failed_attempts[ip] >= 5:
+						print(f"ALERT: {ip} has {failed_attempts[ip]} failed attempts.")
+		else:
+			time.sleep(1)
